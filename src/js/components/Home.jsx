556f841316eceb7348bfd884153fafe5e75fa0c9
beatriz24BCN <beatriz24bcn@hotmail.com>
2026-03-20 20:16:45 +0000

    proyecto finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const Home = () => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+
+  const [todos, setTodos] = useState([
+ 
+  "Drink coffee ☕",
+  "Walk the dog 🐕",
+  "Save the world 🌍",
+  "Eat pizza 🍕"
+]);
+
+
+  return (
+    <div className="container">
+
+      <h1 className="title">todos</h1>
+
+      <div className="todo-container">
+
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && inputValue.trim() !== "") {
+              setTodos([...todos, inputValue]);
+              setInputValue("");
+            }
+          }}
+        />
+
+        <ul>
+
+          {todos.map((task, index) => (
+
+            <li key={index} className="task">
+
+              {task}
+
+              <span
+                className="delete"
+                onClick={() => {
+                  const newTodos = todos.filter((_, i) => i !== index);
+                  setTodos(newTodos);
+                }}
+              >
+                X
+              </span>
+
+            </li>
+
+          ))}
+
+        </ul>
+
+        <div className="counter">
+          {todos.length} items left
+        </div>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default Home;
